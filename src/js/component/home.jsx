3bd3ff14a314be8d729073e00724e53f5887c1ed
Philippe <82619578+phildesignsgfx@users.noreply.github.com>
2023-11-25 03:39:22 +0000

    completedmission

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from "react";
 
 const Home = () => {
 	const [inputValue, setInputValue] = useState("");
-	const [task, setTask] = useState([]);
-	const s = task.length>1 ? "s" : "";
-    let counter = 1;
+	const [tasks, setTasks] = useState([]);
+	const s = tasks.length>1 ? "s" : "";
+	let counter = 1;
 
 	const deleteAll = async () => {
 		await fetch("https://playground.4geeks.com/apis/fake/todos/user/tincho", {
@@ -77,30 +77,20 @@ const Home = () => {
 
 	}, [])
 
-
 	return (
 		<div className="container mt-3 text-center">
-			
-			<h1 className="mt-4">My To Do List</h1>
-			<h5 className="my-3">What you got to do Today?</h5>
-			
-			<ul class="list-group d-flex align-items-center">
-			<li class="list-group-item d-flex align-items-center py-3">
+			<h1 className="mt-4">ToDo</h1>
+			<h5 className="my-3">Task organizer</h5>
+			<ul className="list-group d-flex align-items-center">
+			<li className="list-group-item d-flex align-items-center py-3">
 			     <input
 				  type="text"
-				  class="form-control me-5"
+				  className="form-control me-5"
 				  onChange={(e) => setInputValue(e.target.value)}
 				  placeholder="Type your task"
 				  value={inputValue}
-				  onKeyUp={(e) => {
-					if(e.key === "Enter") {
-						setTask(task.concat(inputValue));
-						setInputValue("");
-					}
-				  }}
 				  aria-label="Recipient's username"/>
-
- <button
+                 <button
 				  type="button"
 				  onClick={addTask}
 				  className="btn btn-primary float-end ms-auto btn-sm btn-pad">
@@ -112,31 +102,30 @@ const Home = () => {
 				  className="btn btn-secondary float-end ms-3 btn-sm btn-pad text-nowrap">
 					Delete All
 				 </button>
-
-
-
 				
 				</li>
 				
 				{tasks.map((data, index) => (
         
-		<li className="list-group-item d-flex align-items-center py-3"
-			key={`${data.id}`}
-		>
-	   
-	   {`${index + 1} - `} {data.label}
-		<button
-		   type="button"
-		   onClick={() => deleteTask(index)}
-		   className="btn btn-danger float-end ms-auto btn-sm">
-		   Delete
-		</button>
-	</li> 
+						 <li className="list-group-item d-flex align-items-center py-3"
+						     key={`${data.id}`}
+						 >
+						
+						{`${index + 1} - `} {data.label}
+						 <button
+							type="button"
+							onClick={() => deleteTask(index)}
+							className="btn btn-danger float-end ms-auto btn-sm">
+							Delete
+						 </button>
+					 </li> 
                  ))}
-				 
 
-				<div className="list-group-item d-flex align-items-center py-3 t-counter">{task.length} task{s} left</div>
+				<div className="list-group-item d-flex align-items-center py-3 t-counter">{tasks.length} task{s} left</div>
 				</ul>
+				
+		{/* </div> */}
+				
 				<div class="bm-pl">	
 	<div class="bm-pl__blob bm-pl__blob--r"></div>
 	<div class="bm-pl__blob bm-pl__blob--g"></div>
